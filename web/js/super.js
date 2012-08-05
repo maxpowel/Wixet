a77f6bb079e5f6de72e640b451cce1e010f8c545
Álvaro García <maxpowel@gmail.com>
2012-08-06 01:04:01 +0200

    xUpdated gitignore

--- web/js/super.js
+++ web/js/super.js
@@ -57,3 +57,25 @@ var User = Backbone.Model.extend({
 	}
 
 });
+var ListView = Backbone.View.extend({
+    
+    initialize: function() {
+        this.options.collection.bind('add',   this.addOne, this);
+        this.options.collection.bind('reset', this.addAll, this);
+        this.options.collection.bind('remove', this.removeOne, this);
+      },
+      
+      addAll: function() {
+    	  var self = this;
+    	  this.options.collection.each(function(element,i){
+    		  self.addOne(element,self);
+          });    	  
+      },
+      
+      addOne: function(element){
+
+      },
+      removeOne: function(folder) {
+    	  
+      }
+});
